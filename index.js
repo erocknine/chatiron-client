a0ab38983e9e702dc1a3b15dfb7357d369ac0991
erocknine <echeungsk@gmail.com>
2020-04-03 11:18:02 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const BASE_URL = "http://dbdea2b7.ngrok.io"
 
 // Deliverable 1: read messages
+fetch(BASE_URL + "/messages")
+.then(response => response.json())
+.then(messages => {
+  messages.data.forEach(message => renderMessage(message))
+})
 
 // Deliverable 2: create message
+
+const send = document.querySelector(`#new-message-form`)
+send.addEventListener("submit", event => {
+  event.preventDefault()
+  const message = {
+    username: event.target.username.value,
+    text: event.target.text.value
+  }
+  fetch('http://dbdea2b7.ngrok.io/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  })
+  .then((response) => response.json())
+  .then((message) => {
+    renderMessage(message);
+  })
+})
+
 
 // Use this helper function to render the messages!
 function renderMessage(message) {
